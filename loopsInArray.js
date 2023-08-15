2c4fbcf5aa9d4be9a6b4c735ab4f370699958050
CRXAhsan <ahsantariq669@gmail.com>
2023-08-15 23:08:16 +0500

    praticing javascript

---
+++ loopsInArray.js
@@ -0,0 +1,12 @@
+let fruit = ["Apple" , "Mange" , "Banana" , "Orange" , "Grapes"];
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+console.log(fruit.length);
+
+for(let i = 0; i < fruit.length; i++){
+    
+    console.log(fruit[i].toUpperCase());
+}
